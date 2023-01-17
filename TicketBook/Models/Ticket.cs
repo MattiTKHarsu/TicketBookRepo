98cb82ad20592c501ea59063d883f0bc1d071867
Matti Harsu <matti.harsu@etteplan.com>
2023-01-17 18:16:45 +0200

    Some small indendation related fixes.

--- TicketBook/Models/Ticket.cs
+++ TicketBook/Models/Ticket.cs
@@ -1,20 +1,20 @@
 ﻿//-----------------------------------------------------------------------------
 //   -- T I C K E T   R E P O S I T O R Y   E X A M P L E   P R O G R A M --
 //   
-//		Company: 		Etteplan Oy
+//      Company:        Etteplan Oy
 //   	
-//		Programmer: 	Matti Harsu
+//      Programmer:     Matti Harsu
 //
 //      Function:       Datamodel for Tickets
 //
-//		Functionality:
+//      Functionality:
 //      This program work as Back-End part for recording servive request.
 //      Application contains 1) Create record, 2) Read record, 3) Upadate
 //      record and 4) Delete record function. In addition all service records
 //      and records regarding defined devices can be listed.
 //
-//		Libraries:
-//		- 
+//      Libraries:
+//      - 
 //
 //-----------------------------------------------------------------------------
 
